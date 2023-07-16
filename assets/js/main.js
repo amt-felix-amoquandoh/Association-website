4d232150461c1e3e3487d49a19f33661ecbb81a8
Felix Ekow Amoquandoh <felix.amoquandoh@amalitech.org>
2023-07-16 00:05:40 +0000

    app update

--- assets/js/main.js
+++ assets/js/main.js
@@ -72,6 +72,10 @@ const swiper = new Swiper(".swiper", {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+      },
     // Make the pagination indicators work
     pagination: {
         el: '.swiper-pagination'
